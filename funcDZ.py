7c232e3453f1dc2518b528fed6f04e295d129283
DopD <Dope_Dod.x@proton.me>
2023-02-25 17:35:49 +0300

    DZ function2 str + int + list + typle -

--- funcDZ.py
+++ funcDZ.py
@@ -1,9 +1,12 @@
 
 
 def fun(fn):
-
     if type(fn) is str:
-        print(fn, ' - Это строка, в ней ', len(fn),'букв.')
+        bk = 0
+        for i in fn:
+            if i.isalpha():
+                bk += 1
+        print(fn, ' - это строка, в ней', bk, 'букв')
     elif type(fn) is int:
         cht = 0
         for i in str(fn):
@@ -11,7 +14,6 @@ def fun(fn):
             if i % 2 == 0:
                 cht += 1
         print(fn, ' - это число, в нем', cht, 'четных символов.')
-
     elif type(fn) is tuple:
         print('кортеж')
     elif type(fn) is list:
@@ -23,8 +25,10 @@ def fun(fn):
             elif i.isdigit():
                 nu += 1
         print(fn, ' - Это список, в нем', buk, '- букв и', nu, '- цифр.')
+
+
 fun(34289766)
-fun('hjghj')
+fun('hjghj568')
 fun([789, 'sdfgssss'])
 fun(('bj',))
 
